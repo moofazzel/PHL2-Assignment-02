3d0cc79e66ddc445456b29e139964a920459df03
moofazzel <mofazzel444@gmail.com>
2025-05-26 22:46:23 +0600

    first commit

---
+++ PostgreSQL_Assignment.sql
@@ -0,0 +1,73 @@
+SELECT * FROM sightings;
+
+-- Problem 1: Register a new ranger
+INSERT INTO
+    rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+-- Problem 2: Count unique species sighted
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+-- Problem 3: Find all sightings where location includes "Pass"
+SELECT * FROM sightings WHERE location LIKE '%Pass%';
+
+-- Problem 4: List rangers with total sightings
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+    LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY
+    r.name;
+
+-- Problem 5: Species that have never been sighted
+SELECT common_name
+FROM species
+WHERE
+    species_id NOT IN (
+        SELECT DISTINCT
+            species_id
+        FROM sightings
+    );
+
+-- Problem 6: Show the most recent 2 sightings
+SELECT
+    common_name,
+    sighting_time,
+    "name"
+from sightings
+    JOIN species USING (species_id)
+    JOIN rangers USING (ranger_id)
+ORDER BY sighting_time DESC
+LIMIT 2;
+
+-- Problem 7: Update species discovered before 1800 to 'Historic'
+UPDATE species
+SET
+    conservation_status = 'Historic'
+WHERE
+    discovery_date < '1800-01-01';
+
+-- Problem 8: Label sighting times as Morning, Afternoon, or Evening
+SELECT
+    sighting_id,
+    CASE
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) < 12 THEN 'Morning'
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) BETWEEN 12 AND 17  THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings;
+
+-- Problem 9: Delete rangers who have never recorded a sighting
+DELETE FROM rangers
+WHERE
+    ranger_id NOT IN (
+        SELECT DISTINCT
+            ranger_id
+        FROM sightings
+    );
---
+++ PostgreSQL_Assignment_tables.sql
@@ -0,0 +1,119 @@
+CREATE DATABASE conservation_db;
+
+-- Create rangers table
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+-- Create species table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE,
+    conservation_status VARCHAR(50) CHECK (
+        conservation_status IN (
+            'Endangered',
+            'Vulnerable',
+            'Least Concern',
+            'Extinct'
+        )
+    )
+);
+
+-- Create sightings table
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT REFERENCES rangers (ranger_id) ON DELETE CASCADE,
+    species_id INT REFERENCES species (species_id) ON DELETE CASCADE,
+    sighting_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
+
+-- Insert data into rangers table
+INSERT INTO
+    rangers (name, region)
+VALUES (
+        'Alice Green',
+        'Northern Hills'
+    ),
+    ('Bob White', 'River Delta'),
+    (
+        'Carol King',
+        'Mountain Range'
+    ),
+    ('Derek Fox', 'Coastal Plains');
+
+-- Insert data into species table
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
+
+-- Insert data into sightings table
+INSERT INTO
+    sightings (
+        ranger_id,
+        species_id,
+        sighting_time,
+        location,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        '2024-05-10 07:45:00',
+        'Peak Ridge',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        '2024-05-12 16:20:00',
+        'Bankwood Area',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        '2024-05-15 09:10:00',
+        'Bamboo Grove East',
+        'Feeding observed'
+    ),
+    (
+        2,
+        1,
+        '2024-05-18 18:30:00',
+        'Snowfall Pass',
+        NULL
+    );
